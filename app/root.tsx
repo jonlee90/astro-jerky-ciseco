da1e1b0d8d4cca2e62651efeefd97551b6df7d3d
Jonathan <jonlee213@gmail.com>
2025-02-22 20:43:07 -0800

    readded GTM

--- app/root.tsx
+++ app/root.tsx
@@ -186,16 +186,37 @@ export function Layout({children}: {children?: React.ReactNode}) {
         <meta name="google-site-verification" content="25YtPW1Ho9GeTDGAmb7ERIzVTKDKWaCkNnpXAs8tlH4" />
         <Meta />
         <Links />
-        
+        {/***********************************************/
+        /**********  EXAMPLE UPDATE STARTS  ************/}
         <Script
-        async
-        type='text/javascript'
-        src='https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=Xx5PGx'
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5BP4M9R5');`,
+          }}
         ></Script>
-        
+        {/**********   EXAMPLE UPDATE END   ************/
+        /***********************************************/}
       </head>
       
       <body className="bg-white">
+        {/***********************************************/
+        /**********  EXAMPLE UPDATE STARTS  ************/}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5BP4M9R5"
+            height="0"
+            width="0"
+            style={{
+              display: 'none',
+              visibility: 'hidden',
+            }}
+          ></iframe>
+        </noscript>
+        {/**********   EXAMPLE UPDATE END   ************/
+        /***********************************************/}
           {data ? (
             <Analytics.Provider
               cart={data.cart}
