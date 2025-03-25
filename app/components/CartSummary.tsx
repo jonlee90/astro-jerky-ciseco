98a681fb80d6f10923b57febce95ec1eb461321c
Jonathan Lee <jonathanlee@Jonathans-MBP.attlocal.net>
2025-03-24 18:46:54 -0700

    removed agree to checkout

--- app/components/CartSummary.tsx
+++ app/components/CartSummary.tsx
@@ -73,26 +73,26 @@ export function CartSummary({cart}: CartSummaryProps) {
         </div>
       </dl>
       <CartAgreementCheckbox toggleDisableButton={toggleDisableButton} />
-      <CartCheckoutActions checkoutUrl={checkoutUrl} disableButton={disableButton} />
+      <CartCheckoutActions checkoutUrl={checkoutUrl} disableButton={false} />
     </section>
   );
 }
 function CartAgreementCheckbox({ toggleDisableButton }: {toggleDisableButton: () => void}) {
   return (
     <div className="flex flex-row mt-2">
-      <input
+     {/* <input
         type="checkbox"
         id="cart-agreement-checkbox"
         className="align-middle mr-2 my-1 size-5"
         onChange={toggleDisableButton}
         aria-labelledby="agreement-label"
-      />
+      />*/} 
       <label
         id="agreement-label"
         htmlFor="cart-agreement-checkbox"
         className="text-fine"
       >
-        I agree with the <a href="/policies/terms-of-service" className="color-logo-red">Terms of Service</a>, <a href="/policies/refund-policy" className="color-logo-red">Returns/Refund/Exchanges Policy</a>, and <a href="/policies/shipping-policy" className="color-logo-red">Shipping and Handling Policy</a>.
+        By checking out, I agree with the <a href="/policies/terms-of-service" className="color-logo-red">Terms of Service</a>, <a href="/policies/refund-policy" className="color-logo-red">Returns/Refund/Exchanges Policy</a>, and <a href="/policies/shipping-policy" className="color-logo-red">Shipping and Handling Policy</a>.
       </label>
     </div>
   );
