21b8aa98083ac945506c53e204fcc09f0fe4d403
Jonathan <jonlee213@gmail.com>
2025-06-30 21:27:54 -0700

    added promo to announcement

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -54,8 +54,8 @@ export function Header({
     prevScrollY.current = y;
   }, [y]);
   const content = [
+    "USE CODE JULY4 FOR 20% OFF",
     "FREE SHIPPING OVER $60",
-    "SAME DAY SHIPPING",
     "Buy 3 bags for $33",
   ];
   return (
